4482fd1eb47757957d81d4c7721875000e3675db
Woojay Poynter <woojay@linbit.com>
2019-07-03 12:41:05 -0700

    lin_nuke.sh also cleans up rsc defs and vol defs

--- lin_nuke.sh
+++ lin_nuke.sh
@@ -1,1 +1,1 @@
-linstor --no-utf8 r list-volumes | grep \/ | awk '{ print $2 " " $4 }' | while read NN RN; do linstor r d $NN $RN; done
+linstor --no-utf8 r list-volumes | grep \/ | awk '{ print $2 " " $4 }' | while read NN RN; do linstor r d $NN $RN; linstor rd d $RN; done
